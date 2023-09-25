e2b6959eba5bb2c363039b22bad4515049eae55e
Manoj S <140539252+Manoj-S-101@users.noreply.github.com>
2023-09-25 11:53:40 -0700

    Update gradecorner1.js

--- gradecorner1.js
+++ gradecorner1.js
@@ -28,14 +28,17 @@ function displayMessage(form){
   function notifying(form){
     let name = document.getElementById("name").value;
     let time = document.getElementById("time").value;
-    document.getElementById("requesting").classList.add("remove");
+    document.getElementById("requesting").style.display = "none";
     document.getElementById("nameOfStudent").innerHTML = name;
     document.getElementById("timeSpecified").innerHTML = time;
     document.getElementById("notification").style.display="inline-block";
   }
-
+function withdrawing(){
+    document.getElementById("notification").style.display="none";
+    document.getElementById("requesting").style.display = "block";
+  }
   function cancel(){
-    document.getElementById("mentor").style.display = "block";
+   document.getElementById("mentor").style.display = "block";
     document.getElementById("question").style.display = "block";
     document.getElementById("mentor-compelete").style.display = "none";
     document.getElementById("chatWithMentor").style.display = "none";
